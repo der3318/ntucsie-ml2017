3fb962f8f471ec9dbb652e2fa06a410972ea2640
b03902015 <b03902015@linux2.csie.ntu.edu.tw>
2017-05-04 11:04:05 +0800

    hw3 fix train.sh

--- hw3/hw3_train.sh
+++ hw3/hw3_train.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
-cp /history/cnn_keras_tensorflow_ver1.py cnn.py -rf
+cp test.csv test100.csv -rf
+
+cp history/cnn_keras_tensorflow_ver1.py cnn.py -rf
 mkdir -p keras_load
 # python3 cnn.py train.csv test.csv out.csv --semi
-python3 cnn.py $1 test10.csv tmp.csv --semi
-rm keras_laod_ver1 -rf
+python3 cnn.py $1 test100.csv tmp.csv --semi
+rm keras_load_ver1 -rf
 cp keras_load keras_load_ver1 -rf
 
-cp /history/cnn_keras_tensorflow_ver2.py cnn.py -rf
+cp history/cnn_keras_tensorflow_ver2.py cnn.py -rf
 mkdir -p keras_load
-python3 cnn.py $1 test10.csv tmp.csv --semi
-rm keras_laod_ver2 -rf
+python3 cnn.py $1 test100.csv tmp.csv --semi
+rm keras_load_ver2 -rf
 cp keras_load keras_load_ver2 -rf
 
-cp /history/cnn_keras_tensorflow_ver3.py cnn.py -rf
+cp history/cnn_keras_tensorflow_ver3.py cnn.py -rf
 mkdir -p keras_load
-python3 cnn.py $1 test10.csv tmp.csv --semi
-rm keras_laod_ver3 -rf
+python3 cnn.py $1 test100.csv tmp.csv --semi
+rm keras_load_ver3 -rf
 cp keras_load keras_load_ver3 -rf
 
-cp /history/cnn_keras_tensorflow_ver4.py cnn.py -rf
+cp history/cnn_keras_tensorflow_ver4.py cnn.py -rf
 mkdir -p keras_load
-python3 cnn.py $1 test10.csv tmp.csv --semi
-rm keras_laod_ver4 -rf
+python3 cnn.py $1 test100.csv tmp.csv --semi
+rm keras_load_ver4 -rf
 cp keras_load keras_load_ver4 -rf
 
-cp /history/cnn_keras_tensorflow_ver5.py cnn.py -rf
+cp history/cnn_keras_tensorflow_ver5.py cnn.py -rf
 mkdir -p keras_load
-python3 cnn.py $1 test10.csv tmp.csv --semi
-rm keras_laod_ver5 -rf
+python3 cnn.py $1 test100.csv tmp.csv --semi
+rm keras_load_ver5 -rf
 cp keras_load keras_load_ver5 -rf
 
-cp /history/cnn_keras_tensorflow_ver6.py cnn.py -rf
+cp history/cnn_keras_tensorflow_ver6.py cnn.py -rf
 mkdir -p keras_load
-python3 cnn.py $1 test10.csv tmp.csv --semi
-rm keras_laod_ver6 -rf
+python3 cnn.py $1 test100.csv tmp.csv --semi
+rm keras_load_ver6 -rf
 cp keras_load keras_load_ver6 -rf
 
